e5765bf0e628bb16e13465d961973b9377b772e0
weeger <romain.weeger@gmail.com>
2021-10-19 15:33:57 +0200

    Logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -23,4 +23,12 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+    }
 }
