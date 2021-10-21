6816d55096d30d3427c3f3e0724b2568d2c78054
weeger <romain.weeger@gmail.com>
2021-10-21 16:04:13 +0200

    Cleanup index

--- assets/js/pages/home/index.js
+++ assets/js/pages/home/index.js
@@ -1,9 +1,9 @@
 import Manager from "../../class/MouseTail/Manager";
-// TODO cleanup
-document.addEventListener('DOMContentLoaded', () => {
-  new Manager();
-})
 
 export default {
   classType: 'pages',
+
+  init() {
+    // TODO new Manager();
+  }
 };
