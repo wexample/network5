e4364a1bb927c054783436de2031391f3e15435b
weeger <romain.weeger@gmail.com>
2021-10-25 09:31:12 +0200

    Remove sensitive data

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,5 +10,5 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php'))
 }
 elseif (method_exists(Dotenv::class, 'bootEnv'))
 {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env.local');
 }
