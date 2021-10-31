3ec735187ecbfcb05c970a2643ebde9dd54fa581
weeger <romain.weeger@gmail.com>
2021-10-31 15:48:39 +0100

    Include

--- src/Wex/BaseBundle/Resources/js/interface/MixinInterface.ts
+++ src/Wex/BaseBundle/Resources/js/interface/MixinInterface.ts
@@ -1,11 +1,9 @@
+import {ServiceMethodsInterface} from "../class/ServiceMethodsInterface";
+
 export default interface MixinInterface {
     name: string
     dependencies?: object
     methods?: ServiceMethodsInterface
     service?: any
     hooks?: object
-}
-
-export interface ServiceMethodsInterface {
-    app?: object
 }
