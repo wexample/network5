f47cdf4ede25e7f7c2194b4d97792e789af5209c
weeger <romain.weeger@gmail.com>
2021-12-11 15:31:43 +0100

    Import only used fa icons

--- src/Wex/BaseBundle/Resources/js/vue/debug-assets-explorer-item.vue
+++ src/Wex/BaseBundle/Resources/js/vue/debug-assets-explorer-item.vue
@@ -1,5 +1,8 @@
 <script>
 import ExplorerItem from './explorer-item';
+import { icon as faIcon, library as faLibrary } from '@fortawesome/fontawesome-svg-core'
+import { faCube, faFile, faColumns } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs } from '@fortawesome/free-brands-svg-icons'
 
 export default {
   extends: ExplorerItem,
@@ -14,20 +17,34 @@ export default {
     };
   },
 
+  mounted() {
+    faLibrary.add(faCube,
+        faFile,
+        faColumns,
+        faVuejs
+    );
+  },
+
   methods: {
     getItemName() {
       return this.type;
     },
 
-    getItemIcon() {
-      return (
-          'fa-' +
-          {
-            component: 'cube',
-            layout: 'columns',
-            page: 'file',
-          }[this.type]
-      );
+    renderItemIcon() {
+      let options;
+
+      if (this.object.renderData.name === 'components/vue') {
+        options = faVuejs;
+      } else {
+
+        options = {
+          component: faCube,
+          layout: faColumns,
+          page: faFile,
+        }[this.type];
+      }
+
+      return faIcon(options).html;
     },
 
     getChildren() {
