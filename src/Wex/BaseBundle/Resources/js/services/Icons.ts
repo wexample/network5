08bf8de6343265c1ecdfdfb5b7d057d8e8b97403
weeger <romain.weeger@gmail.com>
2021-12-13 14:46:46 +0100

    Minor remove

--- src/Wex/BaseBundle/Resources/js/services/Icons.ts
+++ src/Wex/BaseBundle/Resources/js/services/Icons.ts
@@ -2,16 +2,6 @@ import AppService from '../class/AppService';
 import { icon as faIcon, library as faLibrary } from '@fortawesome/fontawesome-svg-core'
 
 export default class IconsService extends AppService {
-  registerHooks() {
-    return {
-      app: {
-        init() {
-          this.app.services.icons.appInit();
-        },
-      },
-    };
-  }
-
   add(icon) {
     faLibrary.add.apply(faLibrary, arguments);
   }
