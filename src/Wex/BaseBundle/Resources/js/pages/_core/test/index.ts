e5325356de1adf5b18a9edc9a530afdd808a5c00
weeger <romain.weeger@gmail.com>
2021-12-23 11:18:10 +0100

    Todos

--- src/Wex/BaseBundle/Resources/js/pages/_core/test/index.ts
+++ src/Wex/BaseBundle/Resources/js/pages/_core/test/index.ts
@@ -2,6 +2,19 @@ import Page from '../../../class/Page';
 
 export default class extends Page {
   async mounted() {
-    console.log('wip...');
+    // TODO Test adaptive rendering
+    // TODO Test non adaptive
+    // TODO Test no js
+    // TODO Test no aggregation
+    // TODO Test icons
+    // TODO Test vue
+    // TODO Test component
+    // TODO Test component in vue
+    // TODO Test responsive
+    // TODO Test color schemes
+    // TODO Test modal
+    // TODO Test modal in modal
+    // TODO Test variables (layout / page / com / modal)
+    // TODO Test translations (layout / page / com / modal)
   }
 }
