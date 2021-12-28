ffd4cc24f89c6875dfe083e2d783e4271bb82b17
weeger <romain.weeger@gmail.com>
2021-12-28 15:35:12 +0100

    Blink rollback

--- src/Wex/BaseBundle/Resources/js/components/test-component.ts
+++ src/Wex/BaseBundle/Resources/js/components/test-component.ts
@@ -1,6 +1,10 @@
 import Component from '../class/Component';
 
 export default class extends Component {
+  protected interval: any;
+  protected onIntervalProxy: Function;
+  protected elBlink: HTMLElement;
+
   async init() {
     await super.init();
 
@@ -8,11 +12,21 @@ export default class extends Component {
 
     let el = this.el.querySelector('.test-component-test-js') as HTMLElement;
     el.style.backgroundColor = 'green';
+
+    this.elBlink = this.el.querySelector('.test-blink');
+    this.onIntervalProxy = this.onInterval.bind(this);
+    this.interval = setInterval(this.onIntervalProxy, 1000);
   }
 
   async exit() {
     await super.exit();
 
     delete this.app.layout.vars.testComponentLoaded;
+  }
+
+  onInterval() {
+    this.elBlink.style.display =
+      this.elBlink.style.display ===
+      'none' ? 'inline' : 'none';
   }
 }
