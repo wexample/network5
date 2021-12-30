9d8bb1b454031cf857f9f982d026eb9a36f12688
weeger <romain.weeger@gmail.com>
2021-12-30 14:40:25 +0100

    Cleanup

--- src/Wex/BaseBundle/Resources/js/pages/_core/test/index.ts
+++ src/Wex/BaseBundle/Resources/js/pages/_core/test/index.ts
@@ -14,11 +14,8 @@ export default class extends TestManagerPage {
     // TODO Test no aggregation
     // TODO Test icons
     // TODO Test vue
-    // TODO Test component
-    // TODO Test component in vue
     // TODO Test responsive (and js responsive displays)
     // TODO Test color schemes
-    // TODO Test modal
     // TODO Test modal in modal
     // TODO Test js helpers
     // TODO Test variables (layout / page / com / modal)
