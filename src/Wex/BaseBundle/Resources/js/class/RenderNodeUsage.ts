44b157f6dabbce18902239deec8d2628dacf409d
weeger <romain.weeger@gmail.com>
2022-01-06 13:25:05 +0100

    Prepare usage constants

---
+++ src/Wex/BaseBundle/Resources/js/class/RenderNodeUsage.ts
@@ -0,0 +1,23 @@
+import AppChild from "./AppChild";
+
+export default abstract class RenderNodeUsage extends AppChild {
+  public static USAGE_ANIMATION: string = 'animation';
+
+  public static USAGE_COLOR_SCHEME: string = 'color-scheme';
+
+  public static USAGE_INITIAL: string = 'initial';
+
+  public static USAGE_RESPONSIVE: string = 'responsive';
+
+  public static USAGE_SHAPE: string = 'shape';
+
+  public static USAGES: string[] = [
+    RenderNodeUsage.USAGE_ANIMATION,
+    RenderNodeUsage.USAGE_COLOR_SCHEME,
+    RenderNodeUsage.USAGE_INITIAL,
+    RenderNodeUsage.USAGE_RESPONSIVE,
+    RenderNodeUsage.USAGE_SHAPE,
+  ];
+
+  public abstract name: string;
+}
