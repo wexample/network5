72876911123dfa010dfea397fce64a748cfcca82
weeger <romain.weeger@gmail.com>
2022-01-11 11:02:43 +0100

    Typing

--- src/Wex/BaseBundle/Entity/Interfaces/AbstractEntityInterface.php
+++ src/Wex/BaseBundle/Entity/Interfaces/AbstractEntityInterface.php
@@ -4,7 +4,13 @@ namespace App\Wex\BaseBundle\Entity\Interfaces;
 
 interface AbstractEntityInterface
 {
+    /**
+     * @return ?int
+     */
     public function getId(): ?int;
 
+    /**
+     * @param int $id
+     */
     public function setId(int $id);
 }
